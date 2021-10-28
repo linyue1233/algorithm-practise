1bb853c8d49fb18eaede1bca2f2d03ae907698a5
linyue1233 <1191609469@qq.com>
2021-10-27 22:40:46 -0400

    dp 62 change two-dimension to one

---
+++ src/dynamicProgramming/JumpGame.java
@@ -0,0 +1,16 @@
+package dynamicProgramming;
+
+public class JumpGame {
+    // 33
+    public static boolean canJump(int[] nums) {
+        if(nums.length == 1) return true;
+        int maxPaths = 0;
+        for(int i = 0;i<nums.length;++i){
+            if(i<=maxPaths){
+                maxPaths = Math.max(maxPaths, nums[i] + i);
+            }
+            if( maxPaths >= nums.length-1) return true;
+        }
+        return false;
+    }
+}
