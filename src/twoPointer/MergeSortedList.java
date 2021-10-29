13377f82f00812e43febe78a7807cd48cd300abe
linyue1233 <1191609469@qq.com>
2021-10-28 23:10:46 -0400

    two pointer 21

---
+++ src/twoPointer/MergeSortedList.java
@@ -0,0 +1,40 @@
+package twoPointer;
+
+
+public class MergeSortedList {
+    // 21
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode preHead = new ListNode(-1);
+        ListNode prePoint = preHead;
+        while(l1!=null && l2!=null){
+            if(l1.val <= l2.val){
+                prePoint.next = l1;
+                l1 = l1.next;
+            }else{
+                prePoint.next = l2;
+                l2 = l2.next;
+            }
+            prePoint = prePoint.next;
+        }
+        prePoint.next = l1 == null ? l2 : l1;
+        return preHead.next;
+    }
+}
