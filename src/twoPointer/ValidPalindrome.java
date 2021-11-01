882273af363e43378467823f1f2fbb5370bed159
linyue1233 <1191609469@qq.com>
2021-10-31 23:09:18 -0400

    two pointer 125

---
+++ src/twoPointer/ValidPalindrome.java
@@ -0,0 +1,27 @@
+package twoPointer;
+
+public class ValidPalindrome {
+    //125
+    public static boolean isPalindrome(String s) {
+        int left = 0, right = s.length()-1;
+        while(left <= right){
+            if( !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }else if( !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }else{
+                if( Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right)) ){
+                   return false;
+                }else{
+                    left++;
+                    right--;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("0P"));
+    }
+}
