cf25afe701bd2d8a768a243df8a4fe10dadf8278
linyue1233 <1191609469@qq.com>
2021-11-03 01:54:20 -0400

    647

---
+++ src/twoPointer/PalindromeSubstring.java
@@ -0,0 +1,22 @@
+package twoPointer;
+
+public class PalindromeSubstring {
+    // 647
+    public int countSubstrings(String s) {
+        int ans = 0, len = s.length();
+        for(int i = 0;i<len;++i){
+            // the number is odd
+            int l = i, r = i;
+            while(l >=0 && r < len && s.charAt(l--) == s.charAt(r++)){
+                ans++;
+            }
+
+            l = i;
+            r = i+1;
+            while(l >=0 && r < len && s.charAt(l--) == s.charAt(r++)){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
