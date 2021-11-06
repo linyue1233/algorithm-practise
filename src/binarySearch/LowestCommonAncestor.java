1e46b3c5558960be705a0ad12449434f09f80fed
linyue1233 <1191609469@qq.com>
2021-11-06 16:33:38 -0400

    BST 236

--- src/binarySearch/LowestCommonAncestor.java
+++ src/binarySearch/LowestCommonAncestor.java
@@ -13,7 +13,19 @@ public class LowestCommonAncestor {
     }
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        return root;
-    }
+        if(root == null) return null;
+        if( root.val == p.val || root.val == q.val){
+            return root;
+        }
 
+        TreeNode leftNode = lowestCommonAncestor(root.left, p ,q);
+        TreeNode rightNode = lowestCommonAncestor(root.right,p,q);
+        if(leftNode != null && rightNode !=null){
+            return root;
+        }else if(leftNode != null){
+            return leftNode;
+        }else{
+            return rightNode;
+        }
+    }
 }
