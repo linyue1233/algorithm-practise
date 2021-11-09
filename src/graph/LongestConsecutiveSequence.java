abbc826e676d6b9eff530b6d54d34ce39db678db
linyue1233 <1191609469@qq.com>
2021-11-08 21:55:35 -0500

    graph 128 map

---
+++ src/graph/LongestConsecutiveSequence.java
@@ -0,0 +1,30 @@
+package graph;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutiveSequence {
+    // 128
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> map = new HashSet<>();
+        for(int item : nums){
+            map.add(item);
+        }
+        int ans = 0,cur = 0;
+        for(int item : map){
+            if( map.contains( item - 1 )){
+                continue;
+            }else{
+                int curNumber = item;
+                int curLenght = 1;
+                while( map.contains( curNumber + 1 ) ){
+                    curLenght++;
+                    curNumber++;
+                }
+                ans = Math.max(ans, curLenght);
+            }
+        }
+        return ans;
+    }
+}
