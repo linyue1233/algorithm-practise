f1d258c7f1e10373b68865c4e95a0dfd4a9dd67f
linyue1233 <1191609469@qq.com>
2021-11-29 23:22:01 -0500

    400 bit

---
+++ src/bitOperation/FindNthDigital.java
@@ -0,0 +1,27 @@
+package bitOperation;
+
+public class FindNthDigital {
+    // 400
+    public static int findNthDigit(int n) {
+        int d = 1;
+        int count = 0;
+        int nine = 9;
+        while (n > (long) d * nine) {
+            n -= d * nine;
+            d++;
+            nine = nine * 10;
+        }
+
+        // determin the num
+        int index = n - 1;
+        int start = (int) Math.pow(10, d - 1);
+        int num = start + index / d;
+        char c = String.valueOf(num).charAt((index) % d);
+        return c - '0';
+    }
+    
+
+    public static void main(String[] args) {
+        findNthDigit(11);
+    }
+}
