688aa2caa769bb11568268c180645eacb001b1e0
linyue1233 <1191609469@qq.com>
2021-12-07 20:17:29 -0500

    #amazon lc 1719 heap

--- src/amazone/MaximumUnits.java
+++ src/amazone/MaximumUnits.java
@@ -2,8 +2,26 @@ package amazone;
 
 import java.util.PriorityQueue;
 
-//public class MaximumUnits {
-//    public int maximumUnits(int[][] boxTypes, int truckSize) {
-//        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1]-b[1]);
-//    }
-//}
+public class MaximumUnits {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        // build max-heap
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[1]-a[1]);
+        for(int[] item : boxTypes){
+            pq.offer(item);
+        }
+        int ans = 0;
+        while(pq.size() != 0 && truckSize > 0){
+            int num = pq.peek()[0];
+            int value = pq.peek()[1];
+            pq.poll();
+            if( num <= truckSize){
+                ans += num * value;
+                truckSize = truckSize - num;
+            }else{
+                ans += truckSize * value;
+                return ans;
+            }
+        }
+        return ans;
+    }
+}
