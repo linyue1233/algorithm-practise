dd70335faf3c453b81b7d5c58043907eb80a7c01
linyue1233 <1191609469@qq.com>
2021-12-07 21:14:35 -0500

    #amazon lc 146 linkedList

---
+++ src/amazone/LRUCache.java
@@ -0,0 +1,37 @@
+package amazone;
+
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCache {
+    //156
+    private int cap;
+    private Map<Integer, Integer> map = new LinkedHashMap<>();
+
+    public LRUCache(int capacity) {
+        this.cap = capacity;
+    }
+
+    public int get(int key) {
+        if (map.keySet().contains(key)) {
+            int value = map.get(key);
+            map.remove(key);
+            map.put(key, value);
+            return value;
+        } else {
+            return -1;
+        }
+    }
+
+    public void put(int key, int value) {
+        if (map.keySet().contains(key)) {
+            map.remove(key);
+        } else if (map.size() == cap) {
+            Iterator<Map.Entry<Integer, Integer>> iterator = map.entrySet().iterator();
+            iterator.next();
+            iterator.remove();
+        }
+        map.put(key, value);
+    }
+}
