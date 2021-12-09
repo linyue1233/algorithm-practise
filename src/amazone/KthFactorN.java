31617df63345ad835c0048ec681c8795884957dc
linyue1233 <1191609469@qq.com>
2021-12-09 00:33:57 -0500

    #amazon lc 1492 math

---
+++ src/amazone/KthFactorN.java
@@ -0,0 +1,24 @@
+package amazone;
+
+public class KthFactorN {
+    // 1492
+    public int kthFactor(int n, int k) {
+        int ans = -1;
+        int cur = 1;
+        for(;cur * cur < n;++cur){
+            if(n%cur == 0 && --k == 0){
+                return cur;
+            }
+        }
+        // same factor as one, if cur^2 != n means cur^2 > n, we need decrease 1
+        if( cur * cur != n){
+            cur--;
+        }
+        for(;cur>=1;cur--){
+            if(n%cur == 0 && --k == 0){
+                return n/cur;
+            }
+        }
+        return -1;
+    }
+}
