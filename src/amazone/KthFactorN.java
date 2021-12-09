811e67acf2a3d07f646f08580352c372c2bc1499
linyue1233 <1191609469@qq.com>
2021-12-09 15:25:42 -0500

    #amazon lc 1401 robot rounded

--- src/amazone/KthFactorN.java
+++ src/amazone/KthFactorN.java
@@ -5,13 +5,14 @@ public class KthFactorN {
     public int kthFactor(int n, int k) {
         int ans = -1;
         int cur = 1;
-        for(;cur * cur < n;++cur){
+        for(;cur * cur <= n;++cur){
             if(n%cur == 0 && --k == 0){
                 return cur;
             }
         }
+        cur--;
         // same factor as one, if cur^2 != n means cur^2 > n, we need decrease 1
-        if( cur * cur != n){
+        if( cur * cur == n){
             cur--;
         }
         for(;cur>=1;cur--){
---
+++ src/amazone/RobotRounded.java
@@ -0,0 +1,31 @@
+package amazone;
+
+public class RobotRounded {
+    //1041
+    public boolean isRobotBounded(String instructions) {
+        int len = instructions.length();
+        int x = 0, y = 0;
+        int initVector = 0;
+        for (int i = 0; i < len; ++i) {
+            if (instructions.charAt(i) == 'L') {
+                initVector -= 90;
+            } else if (instructions.charAt(i) == 'R') {
+                initVector += 90;
+            } else {
+                int tan = Math.abs(initVector % 360);
+                if (tan == 0) {
+                    y += 1;
+                } else if (tan == 90) {
+                    x += 1;
+                } else if (tan == 180) {
+                    y -= 1;
+                } else {
+                    x -= 1;
+                }
+                continue;
+            }
+        }
+        initVector = Math.abs(initVector % 360);
+        return initVector != 0 || (x == 0 && y == 0);
+    }
+}
