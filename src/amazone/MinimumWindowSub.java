35cccf3e72ea4600eb9b5535fccabf57ed92b565
linyue1233 <1191609469@qq.com>
2021-12-18 21:46:19 -0500

    #amazon lc 76

---
+++ src/amazone/MinimumWindowSub.java
@@ -0,0 +1,36 @@
+package amazone;
+
+public class MinimumWindowSub {
+    //76
+    public String minWindow(String s, String t) {
+        int len = s.length(), left = 0, count = 0;
+        int minLeft = 0, minSize = len + 1;
+        boolean[] flag = new boolean[128];
+        int[] store = new int[128];
+        for(int i = 0;i<t.length();++i){
+            flag[t.charAt(i)] = true;
+            store[t.charAt(i)]++;
+        }
+
+        for(int i =0;i<len;++i){
+            if(flag[s.charAt(i)]){
+                if(--store[s.charAt(i)] >= 0){
+                    count++;
+                }
+                while(count == t.length()){
+                    if(i-left+1 < minSize){
+                        minSize = i-left+1;
+                        minLeft = left;
+                    }
+                    // such as aaaaabc ,abc
+                    // --store[s.charAt(i)] will decrease the extra char
+                    if(flag[s.charAt(left)] && ++store[s.charAt(left)] > 0){
+                        count--;
+                    }
+                    left++;
+                }
+            }
+        }
+        return minSize>len?"":s.substring(minLeft,minLeft+minSize);
+    }
+}
