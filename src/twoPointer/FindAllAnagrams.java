6fc5f91686c9cf3229e5e6472c4bbeb06ea0a5bd
linyue1233 <1191609469@qq.com>
2021-12-20 00:25:43 -0500

    #amazon lc 438 slide window

--- src/twoPointer/FindAllAnagrams.java
+++ src/twoPointer/FindAllAnagrams.java
@@ -1,39 +1,61 @@
 package twoPointer;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FindAllAnagrams {
     // 438
-    public static List<Integer> findAnagrams(String s, String p) {
-        int[] letters = new int[26];
-        for (int i = 0; i < p.length(); ++i) {
-            char c = p.charAt(i);
-            letters[c - 'a']++;
-        }
-
+    public List<Integer> findAnagrams(String s, String p) {
         List<Integer> ans = new ArrayList<>();
-        int left;
-        int[] tempStore = new int[26];
-        int len = p.length();
-        for (int end = len - 1; end < s.length(); ++end) {
-            for (left = end - len + 1; left <= end; ++left) {
-                char c = s.charAt(left);
-                tempStore[c - 'a']++;
-                if (letters[c - 'a'] < tempStore[c - 'a']) {
-                    break;
-                }
+        int n = s.length(), m = p.length();
+        if(n<m){
+            return ans;
+        }
+        int[] storeS = new int[26];
+        int[] storeP = new int[26];
+        for(int i = 0;i<m;++i){
+            storeS[s.charAt(i)-'a']++;
+            storeP[p.charAt(i)-'a']++;
+        }
+        if(Arrays.equals(storeS,storeP)){
+            ans.add(0);
+        }
+        for(int i = m;i<n;++i){
+            storeS[s.charAt(i-m)-'a']--;
+            storeS[s.charAt(i)-'a']++;
+            if(Arrays.equals(storeS,storeP)){
+                ans.add(i-m+1);
             }
-            if (left <= s.length() && left == end+1 && tempStore[s.charAt(end) -'a'] == letters[s.charAt(end) -'a']) {
-                ans.add(end - len + 1);
-            }
-            tempStore = new int[26];
         }
         return ans;
     }
+//    public static List<Integer> findAnagrams(String s, String p) {
+//        int[] letters = new int[26];
+//        for (int i = 0; i < p.length(); ++i) {
+//            char c = p.charAt(i);
+//            letters[c - 'a']++;
+//        }
+//
+//        List<Integer> ans = new ArrayList<>();
+//        int left;
+//        int[] tempStore = new int[26];
+//        int len = p.length();
+//        for (int end = len - 1; end < s.length(); ++end) {
+//            for (left = end - len + 1; left <= end; ++left) {
+//                char c = s.charAt(left);
+//                tempStore[c - 'a']++;
+//                if (letters[c - 'a'] < tempStore[c - 'a']) {
+//                    break;
+//                }
+//            }
+//            if (left <= s.length() && left == end+1 && tempStore[s.charAt(end) -'a'] == letters[s.charAt(end) -'a']) {
+//                ans.add(end - len + 1);
+//            }
+//            tempStore = new int[26];
+//        }
+//        return ans;
+//    }
 
     // 1 2 3 5
-    public static void main(String[] args) {
-        System.out.println(findAnagrams("abacbabc", "abc"));
-    }
 }
