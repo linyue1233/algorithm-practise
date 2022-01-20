df264ffe803b07847b8a088c25ccc559fd1e4413
linyue1233 <1191609469@qq.com>
2022-01-20 18:14:40 -0500

    #amazon lc 669

---
+++ src/tree/TrimBST.java
@@ -0,0 +1,39 @@
+package tree;
+
+public class TrimBST {
+    // 669
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.left = left;
+            this.right = right;
+            this.val = val;
+        }
+
+    }
+
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val < low) {
+            return trimBST(root.right, low, high);
+        }
+        if (root.val > high) {
+            return trimBST(root.left, low, high);
+        }
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+        return root;
+    }
+}
