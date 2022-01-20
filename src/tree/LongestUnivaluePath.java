4beecb39a068408009578746f2578ed1d85b2f17
linyue1233 <1191609469@qq.com>
2022-01-19 20:58:02 -0500

    #amazon lc 687

---
+++ src/tree/LongestUnivaluePath.java
@@ -0,0 +1,40 @@
+package tree;
+
+public class LongestUnivaluePath {
+    // 687
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    private int path = 0;
+
+    public int longestUnivaluePath(TreeNode root) {
+        dfs(root);
+        return path;
+    }
+
+    private int dfs(TreeNode root){
+        if (root == null) return 0;
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        int leftPath = root.left != null && root.left.val == root.val ? left + 1 : 0;
+        int rightPath = root.right != null && root.right.val == root.val ? right + 1 : 0;
+        path = Math.max(path, leftPath + rightPath);
+        return Math.max(leftPath, rightPath);
+    }
+}
