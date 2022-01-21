fd2cb718a4971aa9535f768ccf2043030f9f5b2b
linyue1233 <1191609469@qq.com>
2022-01-21 16:54:55 -0500

    # test

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -6,21 +6,18 @@ import java.util.*;
 
 public class Anagrams {
     public  static int lengthOfLongestSubstring (String s) {
-        // write code here
-        // slide window
-        int len = s.length();
-        int maxLength = 0;
-        Map<Character,Integer> map = new HashMap<>();
-        int end  = 0;
-        while( end < len){
-            char c = s.charAt(end);
-            if(map.containsKey(c)){
-                maxLength = Math.max(maxLength, end - map.get(c));
+        int ans = 0, start = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0;i<s.length();++i){
+            if(map.containsKey(s.charAt(i))){
+                // update new start, jump the repeated letter
+                // make sure the right of start has no repeated letter
+                start = Math.max(start, map.get(s.charAt(i)) + 1);
             }
-            map.put(c,end);
-            end++;
+            ans = Math.max(ans,i - start + 1);
+            map.put(s.charAt(i),i);
         }
-        return maxLength;
+        return ans;
     }
 
 //010 010
