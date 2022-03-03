b5e10faeb9d83303ca5603016b203dcb4c1775dd
linyue1233 <1191609469@qq.com>
2022-03-03 16:52:51 -0500

    # Amazon practise interview ##  210 topo

--- src/amazone/CourseSchedule.java
+++ src/amazone/CourseSchedule.java
@@ -6,34 +6,37 @@ public class CourseSchedule {
     // 210
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         int[] ans = new int[numCourses];
-        List<List<Integer>> edges = new ArrayList<>();
+        // store the pre courses number
         int[] store = new int[numCourses];
+        List<List<Integer>> edges = new ArrayList<>();
         int index = 0;
-        for(int i = 0;i<numCourses;++i){
+        for(int i = 0;i<numCourses;i++){
             edges.add(new ArrayList<>());
         }
-        for (int[] item : prerequisites) {
+        for(int item[] : prerequisites){
             edges.get(item[1]).add(item[0]);
-            ++store[item[0]];
+            store[item[0]]++;
         }
         Deque<Integer> deque = new LinkedList<>();
         for(int i = 0;i<numCourses;++i){
             if(store[i] == 0){
-                deque.offer(i);
+                deque.offerLast(i);
                 ans[index++] = i;
             }
         }
         while(index != numCourses && !deque.isEmpty()){
-            // pop the course which doesn't need pre-course
-            int temp = deque.poll();
-            for(int item : edges.get(temp)){
-                store[item]--;
-                if(store[item] == 0){
-                    ans[index++] = item;
-                    deque.offer(item);
+            int size = deque.size();
+            for(int i = 0;i<size;++i){
+                int temp = deque.pollFirst();
+                for(int item : edges.get(temp)){
+                    store[item]--;
+                    if(store[item] == 0){
+                        deque.offerLast(item);
+                        ans[index++] = item;
+                    }
                 }
             }
         }
-        return index == numCourses?ans:new int[]{};
+        return index == numCourses ? ans : new int[]{};
     }
 }
