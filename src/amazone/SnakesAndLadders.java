3026fb0e9448032388709798d3e27928b8f752cd
linyue1233 <1191609469@qq.com>
2022-03-24 23:34:18 -0400

    # Amazon practise interview ##  909 bfs dice

--- src/amazone/SnakesAndLadders.java
+++ src/amazone/SnakesAndLadders.java
@@ -1,34 +1,35 @@
 package amazone;
 
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class SnakesAndLadders {
     // 909
     public int snakesAndLadders(int[][] board) {
-        //[cur+1,min(cur+6,n^2)]
         int n = board.length;
-        Queue<int[]> queue = new LinkedList<>();
+        Deque<int[]> deque = new LinkedList<>();
         boolean[] visited = new boolean[n*n+1];
         // the first means the index of matrix, the second means movement
-        queue.offer(new int[]{1,0});
-        while(!queue.isEmpty()){
-            int[] temp = queue.poll();
+        deque.offer(new int[]{1,0});
+
+        while( !deque.isEmpty()){
+            int[] temp = deque.pollFirst();
             for(int i = 1;i<=6;++i){
-                int next = temp[0]+i;
-                // out of boundary
-                if(next > n*n){
+                int next = temp[0] + i;
+                if(next > n * n){
                     break;
                 }
-                int[] index = transformMatrix(next,n);
-                if(board[index[0]][index[1]] > 0){
-                    next = board[index[0]][index[1]];
+                int[] grid = transformMatrix(next,n);
+                if(board[grid[0]][grid[1]] > 0){
+                    next = board[grid[0]][grid[1]];
                 }
-                if(next == n*n){
+                if( next == n*n){
                     return temp[1]+1;
                 }
-                if( !visited[next] ){
-                    queue.offer( new int[]{next,temp[1]+1} );
+                // avoid repeat circle
+                if( !visited[next]){
+                    deque.offerLast(new int[]{next,temp[1]+1});
                     visited[next] = true;
                 }
             }
@@ -36,6 +37,7 @@ public class SnakesAndLadders {
         return -1;
     }
 
+    // transfer the index
     private int[] transformMatrix(int next, int n){
         int r = (next-1) / n, c = (next-1) % n;
         if(r%2==1){
