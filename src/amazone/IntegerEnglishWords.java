09855d38f383cc2c8a7aaff396966c87a824c07d
linyue1233 <1191609469@qq.com>
2022-03-30 15:48:07 -0400

    # Amazon practise interview ##  273 simulation

---
+++ src/amazone/IntegerEnglishWords.java
@@ -0,0 +1,34 @@
+package amazone;
+
+public class IntegerEnglishWords {
+    //273
+    String[] words = new String[] {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+            "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen",
+            "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
+    public String numberToWords(int num) {
+        if(num==0) {
+            return "Zero";
+        }
+        return helper(num);
+    }
+
+    private String helper(int num){
+        StringBuilder sb = new StringBuilder();
+        if( num >= 1000000000){
+            sb.append(helper(num / 1000000000) + " Billion ").append(helper(num%1000000000));
+        }else if(num >= 1000000){
+            sb.append(helper(num / 1000000) + " Million ").append(helper(num%1000000));
+        }else if(num >= 1000){
+            sb.append(helper(num / 1000) + " Thousand ").append(helper(num%1000));
+        }else if(num >= 100){
+            sb.append(helper(num / 100) + " Hundred ").append(helper(num%100));
+        }else if( num >= 20){
+            sb.append(words[(num - 20)/10+20]).append(" ").append(words[num%10]);
+        }else{
+            sb.append(words[num]);
+        }
+
+        return sb.toString().trim();
+    }
+}
