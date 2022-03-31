2c9669777d790a4de889258301804b7176158a19
linyue1233 <1191609469@qq.com>
2022-03-31 16:14:03 -0400

    # Amazon practise interview ##  273 simulation

--- src/amazone/IntegerEnglishWords.java
+++ src/amazone/IntegerEnglishWords.java
@@ -16,13 +16,13 @@ public class IntegerEnglishWords {
     private String helper(int num){
         StringBuilder sb = new StringBuilder();
         if( num >= 1000000000){
-            sb.append(helper(num / 1000000000) + " Billion ").append(helper(num%1000000000));
+            sb.append(helper(num / 1000000000)).append(" Billion ").append(helper(num%1000000000));
         }else if(num >= 1000000){
-            sb.append(helper(num / 1000000) + " Million ").append(helper(num%1000000));
+            sb.append(helper(num / 1000000)).append(" Million ").append(helper(num%1000000));
         }else if(num >= 1000){
-            sb.append(helper(num / 1000) + " Thousand ").append(helper(num%1000));
+            sb.append(helper(num / 1000)).append(" Thousand ").append(helper(num%1000));
         }else if(num >= 100){
-            sb.append(helper(num / 100) + " Hundred ").append(helper(num%100));
+            sb.append(helper(num / 100)).append(" Hundred ").append(helper(num%100));
         }else if( num >= 20){
             sb.append(words[(num - 20)/10+20]).append(" ").append(words[num%10]);
         }else{
